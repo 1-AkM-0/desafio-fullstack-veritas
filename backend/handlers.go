d11a370172209845c55ff1364f8033c090eac162
Pedro Yan <thepe880@gmail.com>
2025-11-12 02:50:49 -0300

    refactor(cors): transformar o enableCors em middleware e implementa-lo

--- backend/handlers.go
+++ backend/handlers.go
@@ -20,10 +20,18 @@ func NewServer() *Server {
 	}
 }
 
-func enableCors(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
-	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+func (s *Server) enableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 func (s *Server) RegisterRoutes() http.Handler {
--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	server := NewServer()
 	router := server.RegisterRoutes()
+	handler := server.enableCors(router)
 	fmt.Println("Server rodando na porta 5000")
-	log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServe(":5000", handler))
 }
